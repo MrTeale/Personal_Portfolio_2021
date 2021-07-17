a919049dd8444ccc775fbf3ff0d008013e2e8e01
Lachlan Teale <lachlan.teale@gmail.com>
2021-07-18 00:05:38 +1000

    FIX: Margins on Right Side when Scrolling
    Looks cleaner to have it finish in line with the rest of content rather than edge of screen

--- src/Skills.js
+++ src/Skills.js
@@ -9,10 +9,10 @@ function Skills() {
             <h2 class="text-9xl mx-10 ml-80 text-ironmanred">PYTHON</h2>
             <h2 class="text-9xl mx-10 stroke text-transparent hover:text-black">TENSORFLOW</h2>
             <h2 class="text-9xl mx-10 stroke text-transparent hover:text-black">HTML/CSS/REACT</h2>
-            <h2 class="text-9xl mx-10 stroke text-transparent hover:text-black">AWS/GCP/AZURE</h2>
+            <h2 class="text-9xl mx-10 mr-80 stroke text-transparent hover:text-black">AWS/GCP/AZURE</h2>
         </div>
 
-        <div class="mx-80 pt-5 border-t-2 h-200 border-customgray">
+        <div class="mx-80 pt-5 border-t-2 h-120 border-customgray">
             <h3 class="text-2xl mt-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis interdum, metus vel suscipit elementum, leo justo fringilla arcu, id dictum libero est vel ante. Ut viverra varius ligula, eget gravida erat sodales ac. Vivamus maximus diam et sapien tincidunt, vitae ornare justo malesuada. In massa dui, euismod ac quam sed, interdum maximus elit. Donec vestibulum purus elit, et scelerisque felis consectetur id. Nunc sagittis urna tristique erat feugiat, nec lobortis nunc finibus. Donec porta sodales auctor. Curabitur condimentum enim in pellentesque imperdiet. Nullam facilisis urna eu dolor congue eleifend. Aenean maximus commodo enim id hendrerit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Morbi vehicula consequat consequat.</h3>
         </div>
    </div>
