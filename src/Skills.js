7c0bbda3263e6c232bf75fb0e96aab7ca1f97ace
Lachlan Teale <lachlan.teale@gmail.com>
2021-07-20 23:31:30 +1000

    FEAT: Skills Section updated to allow dynamic text

--- src/Skills.js
+++ src/Skills.js
@@ -1,22 +1,43 @@
+import React, { Component } from 'react';
 import './Skills.css';
 
-function Skills() {
-  return (
-    <div id="skills" className="Skills" class="w-auto lg:mt-44 pt-12">
+const descriptions = {
+  python: "PYTHON HERE",
+  tensorflow: "TENSORFLOW HERE",
+  webdev: "WEBDEV HERE",
+  cloud: "CLOUD HERE"
+};
+
+export default class Skills extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      skillSelected: "python",
+      activeClass: "text-9xl mx-10 text-ironmanred",
+      otherClass: "text-9xl mx-10 stroke text-transparent hover:text-black"
+    }
+  }
+
+  handleClick = (i) => {
+    this.setState({ skillSelected: i });
+  };
+
+  render() {
+    const skills = [{ "key": "python", "text": "PYTHON" }, { "key": "tensorflow", "text": "TENSORFLOW" }, { "key": "webdev", "text": "HTML/CSS/REACT" }, { "key": "cloud", "text": "AWS/GCP/AZURE" }];
+    return (
+      <div id="skills" className="Skills" class="w-auto lg:mt-44 pt-12">
         <h1 class="text-6xl text-black pb-5 border-b-2 border-customgray mx-80">SKILLS & TOOLS</h1>
-        
+
         <div class="scrollbarhide mt-8 mb-4 flex overflow-x-scroll pt-8 overflow-y-hidden">
-            <h2 class="text-9xl mx-10 ml-80 text-ironmanred">PYTHON</h2>
-            <h2 class="text-9xl mx-10 stroke text-transparent hover:text-black">TENSORFLOW</h2>
-            <h2 class="text-9xl mx-10 stroke text-transparent hover:text-black">HTML/CSS/REACT</h2>
-            <h2 class="text-9xl mx-10 mr-80 stroke text-transparent hover:text-black">AWS/GCP/AZURE</h2>
+          {skills.map((skill, index, skills) => {
+            return <h2 class={(index === 0 ? "ml-80 ": "") + (index === skills.length - 1 ? "mr-80 ": "") + (this.state.skillSelected === skill.key ? this.state.activeClass : this.state.otherClass)} onClick={() => this.handleClick(skill.key)}>{skill.text}</h2>})
+          }
         </div>
 
         <div class="mx-80 pt-5 border-t-2 h-120 border-customgray">
-            <h3 class="text-2xl mt-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis interdum, metus vel suscipit elementum, leo justo fringilla arcu, id dictum libero est vel ante. Ut viverra varius ligula, eget gravida erat sodales ac. Vivamus maximus diam et sapien tincidunt, vitae ornare justo malesuada. In massa dui, euismod ac quam sed, interdum maximus elit. Donec vestibulum purus elit, et scelerisque felis consectetur id. Nunc sagittis urna tristique erat feugiat, nec lobortis nunc finibus. Donec porta sodales auctor. Curabitur condimentum enim in pellentesque imperdiet. Nullam facilisis urna eu dolor congue eleifend. Aenean maximus commodo enim id hendrerit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Morbi vehicula consequat consequat.</h3>
+          <h3 class="text-2xl mt-5">{descriptions[this.state.skillSelected]}</h3>
         </div>
-   </div>
-  );
-}
-
-export default Skills;
+      </div>
+    );
+  }
+};
