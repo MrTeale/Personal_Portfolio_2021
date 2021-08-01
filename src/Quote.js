90cf2f50c1d3c542227d85c295c54d262ffb8de7
Lachlan Teale <lachlan.teale@gmail.com>
2021-08-01 16:10:37 +1000

    FEAT: Quote Responsive Styling

--- src/Quote.js
+++ src/Quote.js
@@ -2,12 +2,12 @@ import './Quote.css';
 
 function Quote() {
   return (
-    <div id="quote" className="Quote" class="lg:mx-20 mt-36">
-        <h1 class="uppercase text-9xl text-ironmanred text-left">“Talent means <span class="stroke text-transparent">nothing</span>,</h1>
-        <h1 class="uppercase text-9xl text-ironmanred text-right pr-40">while <span class="stroke text-transparent">experience</span>, </h1>
-        <h1 class="uppercase text-9xl text-ironmanred text-center">acquired in <span class="stroke text-transparent">humility</span> and </h1>
-        <h1 class="uppercase text-9xl text-ironmanred text-right">with <span class="stroke text-transparent">hard work</span>, means</h1>
-        <h1 class="uppercase text-12xl -mt-10 text-ironmanred text-left pl-60">everything.”</h1>
+    <div id="quote" className="Quote" class="mx-2 sm:mx-10 md:mx-28 lg:mx-20 md:pt-8 lg:mt-16 2xl:mt-36">
+        <h1 class="uppercase xl:text-9xl lg:text-8xl md:text-7xl sm:text-6xl text-5xl text-ironmanred text-left">“Talent means <span class="stroke text-transparent">nothing</span>,</h1>
+        <h1 class="uppercase xl:text-9xl lg:text-8xl md:text-7xl sm:text-6xl text-5xl text-ironmanred text-right xl:pr-40 lg:pr-32 md:pr-12">while <span class="stroke text-transparent">experience</span>, </h1>
+        <h1 class="uppercase xl:text-9xl lg:text-8xl md:text-7xl sm:text-6xl text-5xl text-ironmanred text-center">acquired in <span class="stroke text-transparent">humility</span> and </h1>
+        <h1 class="uppercase xl:text-9xl lg:text-8xl md:text-7xl sm:text-6xl text-5xl text-ironmanred text-right">with <span class="stroke text-transparent">hard work</span>, means</h1>
+        <h1 class="uppercase xl:text-12xl lg:text-9xl md:text-8xl sm:text-7xl text-6xl text-ironmanred text-left xl:pl-60 lg:pl-32 md:pl-12 sm:pl-20 pl-24">everything.”</h1>
    </div>
   );
 }
